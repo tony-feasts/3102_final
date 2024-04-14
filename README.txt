5975d4b408480e49b8244c3db30906c1e2a03791
tony-feasts <aug2102@columbia.edu>
2024-04-14 18:28:46 -0400

    create server mongodb link, define data format, & add request handlers

---
+++ README.txt
@@ -0,0 +1,9 @@
+1. created a repo for the final on github
+2. made client and server directories in the cloned repo
+3. copied the contents from the midterm to the client folder
+4. installed react-scripts and used node to test the front-end code locally
+5. initialized the server directory with npm; installed cors, express, & mongoose
+6. set up mongodb cluster with database with notes collection
+7. implement backend index.js**
+8. add proxy to client's package.json
+
--- server/index.js
+++ server/index.js
@@ -1,1 +1,43 @@
-var express = require('express'); 
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const uri = 'mongodb+srv://spongebob:5Om5unxDmqyRioRj@keeper.viyupku.mongodb.' +
+'net/?retryWrites=true&w=majority&appName=Keeper';
+mongoose.connect(uri);
+
+const noteSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+app.get('/api', async (req, res) => {
+  const notes = await Note.find();
+  res.json(notes);
+});
+
+app.post('/api', async (req, res) => {
+  const { title, content } = req.body;
+  const newNote = new Note({ title, content });
+  await newNote.save();
+  res.status(201).json(newNote);
+});
+
+app.delete('/api/:id', async (req, res) => {
+  await Note.findByIdAndRemove(req.params.id);
+  res.status(204).send();
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
